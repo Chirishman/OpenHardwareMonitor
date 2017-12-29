7c04186252ea14e2cdb7eb46bf421a18e2b94ad7
Chirishman <Chirishman@users.noreply.github.com>
2017-12-29 12:39:44 -0500

    Initial commit

---
+++ OpenHardwareMonitor/Public/Measure-CPUTemperature.ps1
@@ -0,0 +1,42 @@
+﻿function Measure-CPUTemperature {
+    Param(
+        [OpenHardwareMonitor.Hardware.Computer]$HardwareMonitor = (New-HardwareMonitor -CPUEnabled)
+    )
+    
+    $HardwareMonitor.Hardware.Update()
+    $HardwareMonitor.Hardware.sensors |
+        ?{$_.SensorType -eq 'Temperature' -and $_.Identifier -match 'cpu'} |
+            Select @{
+                N='CPU Core'
+                E={
+                    $ID = [int]($_.Identifier -split '/')[-1]
+                    if (!$ID){
+                        "CPU Package"
+                    } else {
+                        "CPU Core #$ID"
+                    }
+                }
+            },@{
+                N='CPU Core ID'
+                E={
+                    [int]($_.Identifier -split '/')[-1]
+                }
+            },@{
+                N='Current Temperature'
+                E={
+                    $_.Value
+                }
+            },@{
+                N='Minimum Observed'
+                E={
+                    $_.Min
+                }
+            },@{
+                N='Maximum Observed'
+                E={
+                    $_.Max
+                }
+            } |
+                Sort 'CPU Core ID' |
+                    Select 'CPU Core','Current Temperature','Minimum Observed','Maximum Observed'
+}
---
+++ OpenHardwareMonitor/Public/New-HardwareMonitor.ps1
@@ -0,0 +1,28 @@
+﻿function New-HardwareMonitor {
+    Param(
+        [switch]$MainboardEnabled,
+        [switch]$CPUEnabled,
+        [switch]$RAMEnabled,
+        [switch]$GPUEnabled,
+        [switch]$FanControllerEnabled,
+        [switch]$HDDEnabled
+    )
+    
+    $HardwareMonitor = [OpenHardwareMonitor.Hardware.Computer]::new()
+    
+    $MonitorSwitches = @{
+        MainboardEnabled = $MainboardEnabled
+        CPUEnabled = $CPUEnabled
+        RAMEnabled = $RAMEnabled
+        GPUEnabled = $GPUEnabled
+        FanControllerEnabled = $FanControllerEnabled
+        HDDEnabled = $HDDEnabled
+    }
+    
+    $MonitorSwitches.GetEnumerator() | %{
+        $HardwareMonitor.($_.Key) = $_.Value
+    }
+    $HardwareMonitor.Open()
+
+    $HardwareMonitor
+}
